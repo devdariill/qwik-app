7a29b44b43f179eb0a405946b2866e61281e7380
nATOr <gowluonade@gmail.com>
2023-03-17 07:06:54 -0500

    0

--- src/routes/apis.ts
+++ src/routes/apis.ts
@@ -17,18 +17,3 @@ export const clickSearch = server$((search) => {
   console.log('tempCount', search)
   return new Date()
 })
-
-export const clickFetch = server$(async (search) => {
-  console.log('Task', search)
-  if (!search) return
-  console.log('entro')
-  const url = new URL('https://omdbapi.com/')
-  url.searchParams.set('s', search)
-  url.searchParams.set('apikey', '3067b2f9')
-  console.log(url)
-  //   const URL = `http://omdbapi.com/?s=${search.value}&apikey=3067b2f9`
-  const res = await fetch(url)
-  const data = await res.json()
-  const list = data.Search as Item[]
-  return list ?? []
-})
